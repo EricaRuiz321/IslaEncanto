92ed8c99d76c77e3f8e630b82567f6b042929f9a
EricaRuiz321 <ericaruiz0207@gmail.com>
2025-09-24 10:53:17 -0500

    Update main.py

--- routes/main.py
+++ routes/main.py
@@ -5,10 +5,7 @@ from models.baseDatos import nuevaHabitacion
 main_bp = Blueprint('main', __name__)
 
 #Rutas Home ---------------------------------------------------------
-@main_bp.route("/health")
-def health():
-    return {"status": "ok"}, 200
-
+ 
 @main_bp.route('/')
 def home():
     return render_template('home/Home.html')
