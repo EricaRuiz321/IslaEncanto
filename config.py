1fb05aaf7ba12d0c070c5fc1c3756a31c01dfe92
EricaRuiz321 <ericaruiz0207@gmail.com>
2025-10-09 07:17:07 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://erica:ericac@isladigital.xyz:3311/f58_erica')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_unique_and_secret_key')  # Clave secreta para sesiones
     # Google OAuth2 credentials (set these env vars to enable Google login locally)
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', '')
