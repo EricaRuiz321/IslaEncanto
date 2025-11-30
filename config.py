08302f8ce71f88f87c9203e1ccac1adc0e60afe1
EricaRuiz321 <ericaruiz0207@gmail.com>
2025-11-30 14:27:49 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://erica:ericac@isladigital.xyz:3311/f58_erica')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://erica:ericac@mysql.enlinea.sbs:3311/f58_erica')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_unique_and_secret_key')  # Clave secreta para sesiones
